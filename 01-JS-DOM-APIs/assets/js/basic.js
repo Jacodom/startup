269fed22eecace47ff43f0c47bbf815405b49a45
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-04-25 19:06:23 -0300

    funny jokes

--- 01-JS-DOM-APIs/assets/js/basic.js
+++ 01-JS-DOM-APIs/assets/js/basic.js
@@ -10,3 +10,25 @@ window.onload = function () {
     }, 1000);
   }
 }
+  var button = document.getElementById('button');
+  var response = document.getElementById('response');
+
+
+
+  button.addEventListener('click', function(){
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+
+      if(xhr.readyState == 4 && xhr.status == 200){
+        response.innerHTML = JSON.parse(xhr.responseText).value.joke;
+      }
+      else{
+        response.innerHTML = "failed: "+ xhr.statusText;
+      }
+    }
+
+    xhr.open("GET", "http://api.icndb.com/jokes/random", true);
+    xhr.send();
+
+  });
