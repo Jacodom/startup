697082622f7854b6a697c30325dab5fb77206b54
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-04-25 23:02:39 -0300

    added query to get repos of javascript

--- 01-JS-DOM-APIs/assets/js/basic.js
+++ 01-JS-DOM-APIs/assets/js/basic.js
@@ -12,12 +12,24 @@ window.onload = function () {
 }
   var button = document.getElementById('button');
   var response = document.getElementById('response');
+  var gitBtn = document.getElementById('gitButton');
+
 
   var config = {
     method: "GET",
     url: "asssss",
     async_value: true
   };
+
+
+  var configGit = {
+    method: "GET",
+    url: "https://api.github.com/search/repositories",
+    async_value: true,
+    data:{
+      q: "javascript"
+    }
+  }
 
 
   button.addEventListener('click', function(){
@@ -38,3 +50,39 @@ window.onload = function () {
     xhr.send();
 
   });
+
+
+  var getRepos = function(configGit){
+    var repoList = document.getElementById('repoList');
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState == 4 && xhr.status == 200){
+        var repos = [];
+            repos = JSON.parse(xhr.responseText).items;
+
+            repos.forEach(function (repo) {
+                var li = document.createElement("li");
+                var text = document.createTextNode(repo.full_name);
+                li.appendChild(text);
+                repoList.appendChild(li);
+            });
+      }
+    }
+
+    var url = "";
+
+    if(configGit.data.q != ""){
+      url = configGit.url + "?q=" + configGit.data.q;
+    }else{
+      url = configGit.url;
+    }
+
+    console.log(url);
+
+    xhr.open(configGit.method, url, configGit.async_value);
+    xhr.send();
+  }
+
+  gitBtn.addEventListener("click", getRepos(configGit));
