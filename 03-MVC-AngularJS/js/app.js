ad03be789434e25708d471f1c8f9f7c83600be02
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-05-03 20:49:07 -0300

    movies with details in a different view

--- 03-MVC-AngularJS/js/app.js
+++ 03-MVC-AngularJS/js/app.js
@@ -1,1 +1,24 @@
-angular.module('myApp', []);
+angular.module('myApp', [
+  'ui.router',
+  'angular-storage'
+])
+.config(function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise('/movies');
+
+  $stateProvider
+    .state('movies',{
+      url: '/movies',
+      templateUrl: '../partials/movies.html'
+    })
+    .state('movies.list',{
+      url: '/list',
+      templateUrl: '../partials/movies-list.html',
+      controller: 'MoviesCtrl'
+    })
+    .state('movies.detail', {
+      url: '/detail',
+      templateUrl: '../partials/movies-detail.html',
+      controller: 'MoviesCtrl'
+    });
+});
--- 03-MVC-AngularJS/js/controllers/moviesController.js
+++ 03-MVC-AngularJS/js/controllers/moviesController.js
@@ -1,25 +1,59 @@
 angular.module('myApp')
   .controller('MoviesCtrl', [
     '$scope',
-function ($scope) {
+    '$state',
+    'store',
+function ($scope, $state, store) {
 
-  $scope.yourName = "pepe";
+  if(store.get('movie')){
+    $scope.movie = store.get('movie');
+  }
 
   $scope.movies = [
     {
       title: "Pulp Fiction",
-      year: 1990
+      year: 1990,
+      details: {
+        genre: 'Thriller',
+        actors: ['John Travolta', 'Uma Thurman', 'Samuel Jackson'],
+        imdb: 9.7,
+        director: 'Quentin Tarantino'
+      }
     },
     {
       title: "Reservoir Dogs",
-      year: 1989
+      year: 1989,
+      details: {
+        genre: 'Action',
+        actors: ['Steve Buscemi', 'Tim Roth', 'Harvey Keitel'],
+        imdb: 7.7,
+        director: 'Quentin Tarantino'
+      }
     },
     {
       title: "Django Unchained",
-      year: 2013
+      year: 2013,
+      details: {
+        genre: 'Thriller',
+        actors: ['Lonardo DiCaprio', 'Samuel Jackson', 'Jamie Foxx'],
+        imdb: 8.1,
+        director: 'Quentin Tarantino'
+      }
+    },
+    {
+      title: "A clockwork orange",
+      year: 1985,
+      details: {
+        genre: 'Drama',
+        actors: ['Malcolm McDowell', 'Patrick Magee', 'Michael Bates'],
+        imdb: 8.5,
+        director: 'Stanley Kubrick'
+      }
     }
   ];
 
-
-
+  $scope.seeDetails = function(movie){
+    store.set('movie', movie);
+    $state.go('movies.detail');
+  }
 }]);
