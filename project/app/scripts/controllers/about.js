57d338f00ed34f3ebb2469d196139ab91de71c9d
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-05-10 18:34:32 -0300

    kickstart

---
+++ project/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spotifyClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spotifyClientApp
+ */
+angular.module('spotifyClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
