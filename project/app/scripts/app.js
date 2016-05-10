ec0f1ff71a4e9b2cb761f6d22636184410b02bea
Jacobo Dominguez <dominguezjaco@gmail.com>
2016-05-10 19:11:13 -0300

    added ui-router support

--- project/app/scripts/app.js
+++ project/app/scripts/app.js
@@ -13,23 +13,24 @@ angular
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
+    'ui.router',
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('main');
+
+    $stateProvider
+      .state('main',{
+        url: '/',
+        templateUrl: '../views/main.html',
+        controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+      .state('about',{
+        url: '/about',
+        templateUrl: '../views/about.html',
+        controller: 'AboutCtrl'
       });
+
   });
